e9adbea71b11458da952e23b62d37017811d0139
Ivan Andreev <buddazbs@yandex.ru>
2025-05-12 22:49:45 +0300

    chore(fix): fix double code

--- src/config/api/urlPath.js
+++ src/config/api/urlPath.js
@@ -1,7 +1,17 @@
-export default paths = {
+const config = {
+    baseUrl: "https://nomoreparties.co/v1/wff-cohort-38",
+    headers: {
+        authorization: "33c061d5-f313-4f1b-840d-16309eab437d",
+        "Content-Type": "application/json"
+    }
+};
+
+const urlPaths = {
     me: `${config.baseUrl}/users/me`,
     meAvatar: `${config.baseUrl}/users/me/avatar`,
     cards: `${config.baseUrl}/cards`,
     card: (id) => `${config.baseUrl}/cards/${id}`,
     cardLike: (id) => `${config.baseUrl}/cards/likes/${id}`,
-  };
+};
+
+export  {config, urlPaths};
